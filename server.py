2dcc8282a29c7b73d2944f6ee1e7c12839366ba6
Kyle <kylehawk2@gmail.com>
2018-06-28 08:11:01 -0700

    First Commit

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect, render_template, session, flash
+import re
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+mysql = MySQLConnector(app, 'emailDB')
+app.secret_key = "ThisIsSecret!"
+
+
+print("***************** Emails *******************")
+print mysql.query_db("select * from emails")
+
+
+@app.route('/')
+def index():
+
+    query = "select * from emails"
+    emails = mysql.query_db(query)
+    return render_template('index.html', all_emails=emails)
+
+
+
+@app.route('/success', methods=['POST'])
+def success():
+    query = "select * from emails where email = :email"
+    query2 = "select * from emails"
+    email =  request.form['email']
+    data = {
+             'email': request.form['email']
+           }
+    value = mysql.query_db(query, data)
+    if len(value)==0:
+        valid = False
+        flash('the email you entered was invalid')
+        return redirect('/')
+    else: valid = True
+    value = mysql.query_db(query2, data)
+
+    return render_template('index.html', valid=valid, all_emails=value, email = email)
+
+app.run(debug=True)
